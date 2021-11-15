48206f45079dee903ca7a2bb7021ff0ae7e0bf8d
Nicopal33 <n.palczewski@gmail.com>
2021-11-15 14:00:11 +0100

    commit du 15 novembre

--- app/src/main/java/com/example/goforlunch/ui/MenuActivity.java
+++ app/src/main/java/com/example/goforlunch/ui/MenuActivity.java
@@ -1,13 +1,20 @@
 package com.example.goforlunch.ui;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import androidx.viewbinding.ViewBinding;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+
 import com.example.goforlunch.R;
 import com.example.goforlunch.bottomfragments.MapViewFragment;
 import com.example.goforlunch.databinding.ActivityMenuBinding;
@@ -16,13 +23,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends BaseActivity {
 
-    private ActivityMenuBinding binding;
-    private Fragment mapViewFragment;
-    private Fragment listViewFragment;
-    private Fragment worlkmatesFragment;
+
     BottomNavigationView  bottomNavigationView ;
-    private static final int MAPS_FRAGMENT = 0;
-    private int mFragmentIdentifier;
+
 
 
 
@@ -35,11 +38,12 @@ public class MenuActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.configureBottomView();
+        //this.showFragment(MAPS_FRAGMENT);//
+        setUpNavigation();
 
     }
 
-    public boolean onNavigationBottomItemSelected (MenuItem item) {
+    /*public boolean onNavigationBottomItemSelected (MenuItem item) {
 
         int id = item.getItemId();
 
@@ -47,11 +51,17 @@ public class MenuActivity extends BaseActivity {
             case R.id.mapFragment :
                 this.showFragment(MAPS_FRAGMENT);
                 break;
+            case R.id.listFragment :
+                this.showFragment(LIST_FRAGMENT);
+                break;
+            case R.id.workFragment :
+                this.showFragment(WORK_FRAGMENT);
+                break;
         }
         return true;
-    }
+    }*/
 
-    private void showFragment (int fragmentIdentifier) {
+    /*private void showFragment (int fragmentIdentifier) {
         switch (fragmentIdentifier) {
             case MAPS_FRAGMENT :
                 this.showMapsFragment();
@@ -59,20 +69,34 @@ public class MenuActivity extends BaseActivity {
         }
         mFragmentIdentifier = fragmentIdentifier;
 
-    }
+    }*/
 
-    private void showMapsFragment() {
+    /*private void showMapsFragment() {
         if (this.mapViewFragment == null) this.mapViewFragment = MapViewFragment.newInstance();
+        this.startTransactionFragment(this.mapViewFragment);
+    }
+
+    private void startTransactionFragment(Fragment fragment) {
+        if (!fragment.isVisible()) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.mapFragment,fragment).commit();
+        }
+    }*/
+
+
+
+
+
+    public void setUpNavigation(){
+        bottomNavigationView =findViewById(R.id.nav_bottom_bar );
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id. frameLayout );
+        NavigationUI. setupWithNavController (bottomNavigationView,
+                navHostFragment.getNavController());
     }
 
 
 
-    // 2 - Configure BottomNavigationView Listener
-    private void configureBottomView() {
-        BottomNavigationView bottomNavigationView = binding.navBottomBar;
-        bottomNavigationView.setOnNavigationItemSelectedListener
-                (this::onNavigationBottomItemSelected);
-    }
 
 
 
